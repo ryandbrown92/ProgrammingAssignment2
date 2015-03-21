66a3c48ef4a77bc890b8266a3a137573dac6f236
ryandbrown92 <ryandbrown92@gmail.com>
2015-03-21 17:24:29 -0400

    adding finished cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a list object containing functions for setting the matrix value contained
+## in the object, for returning the stored value, for setting the inverse of the matrix and for returning
+## this inverse. cacheSolve consumes a CacheMatrix object and returns the inverse of the CacheMatrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix consumes a square invertible matrix x and produces a list object whose elements
+## are the functions set(y as square matrix), get(), setinverse(z as square matrix), and getinverse().
+## set changes the value of the matrix stored in the list object. get returns the value of the stored 
+## matrix. setinverse stores the value of the inverse of the stored matrix. getinverse returns the value
+## of the stored matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve consumes a CacheMatrix object and returns the inverse of the stored matrix. If the inverse
+## is not stored in the CacheMatrix object, cacheSolve computes the inverse, stores the inverse in the 
+## CacheMatrix, then returns the computed inverse. Otherwise, cacheSolve returns the inverse value stored
+## in the CacheObject.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
